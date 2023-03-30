709d5cab485b93ad6a0d70b5c750ba8ad5b13f99
wyliemickelson <wyliecoyote910@gmail.com>
2023-03-30 11:25:29 -0700

    cleaned up scraper.js for consistency

--- scraper.js
+++ scraper.js
@@ -12,78 +12,49 @@ export class ScrapingError extends Error {
 }
 
 export function createScraper(liquipediaUrl) {
-  const url = liquipediaUrl;
+  const url = new URL(liquipediaUrl);
   const gameType = getGame();
+  const pageTitle = getPageTitle();
   let pageId;
 
-  // URL functions
-  function stripUrl() {
-    return url
-      .replace("https://", "")
-      .replace("http://", "")
-      .replace("www.", "");
+  function getGame() {
+    const path = url.pathname;
+    // first page redirect should be the game type
+    return path.split("/")[1];
   }
 
   function getPageTitle() {
-    const strippedUrl = stripUrl(url);
-    return strippedUrl.split(`liquipedia.net/${gameType}/`)[1];
-  }
-
-  function getGame(url) {
-    const strippedUrl = stripUrl(url);
-    // first page redirect should be the game type
-    return strippedUrl.split("/")[1];
+    const path = url.pathname;
+    return path.split(`/${gameType}/`)[1];
   }
 
   function validateUrl() {
     const formattedUrl = new URL(url);
     if (formattedUrl.origin != 'https://liquipedia.net') {
-      throw new ScrapingError('Invalid URL');
+      throw new ScrapingError(`Url does not come from liquipedia.net: ${url}`);
     }
   }
 
-  function getPageId(idRequest) {
-    return fetch(idRequest, { headers: headers })
-      .then((res) => res.json())
-      .then((json) => {
-        const pages = Object.keys(json["query"]["pages"]);
-        if (pages.length < 1) {
-          throw new ScrapingError("Could not locate page.");
-        } else if (pages.length > 1) {
-          throw new ScrapingError(
-            `Found ${pages.length} with the name ${liquipediaPageTitle}! Expected only 1.`
-          );
-        }
-        const pageId = pages[0];
-        if (pageId == -1) {
-          throw new ScrapingError(`There is currently no content for page: ${url}.`);
-        }
-        return pageId;
-      })
-  }
-
-  // API Calls
-  async function getHtmlRequest() {
-    const pageRequest = `https://liquipedia.net/${gameType}/api.php?action=parse&format=json&pageid=${pageId}`;
-    return pageRequest;
+  function getHtmlRequest() {
+    return `https://liquipedia.net/${gameType}/api.php?action=parse&format=json&pageid=${pageId}`;
   }
 
   function getIdRequest() {
-    const liquipediaPageTitle = getPageTitle();
-    return `https://liquipedia.net/${gameType}/api.php?action=query&format=json&titles=${liquipediaPageTitle}`;
+    return `https://liquipedia.net/${gameType}/api.php?action=query&format=json&titles=${pageTitle}`;
   }
 
   function getWikiTextReq() {
-    const liquipediaPageTitle = getPageTitle();
-    return `https://liquipedia.net/${gameType}/api.php?action=query&prop=revisions&rvslots=*&titles=${liquipediaPageTitle}&format=json&rvprop=content`;
+    return `https://liquipedia.net/${gameType}/api.php?action=query&prop=revisions&rvslots=*&titles=${pageTitle}&format=json&rvprop=content`;
   }
 
   async function getDataStrings() {
     validateUrl();
     const idRequest = getIdRequest();
-    pageId = await getPageId(idRequest);
-    const htmlRequest = await getHtmlRequest();
+    pageId = await fetchPageId(idRequest);
+    
+    const htmlRequest = getHtmlRequest();
     const htmlStr = await fetchHtml(htmlRequest);
+
     const wikiTextRequest = getWikiTextReq();
     const wikiTextStr = await fetchWikiText(wikiTextRequest);
 
@@ -93,30 +64,42 @@ export function createScraper(liquipediaUrl) {
     }
   }
 
-  function fetchWikiText(wikiTextRequest) {
-    return fetch(wikiTextRequest, { headers: headers })
-      .then((res) => res.json())
-      .then((json) => {
-        let wikiTextStr = json["query"]["pages"][`${pageId}`]["revisions"][0]["slots"]["main"]["*"];
-        if (!wikiTextStr || wikiTextStr.length === 0) {
-          throw new ScrapingError(`Couldn't parse wikitext from ${url}`);
-        }
-
-        return wikiTextStr;
-      })
+  async function fetchPageId(idRequest) {
+    const res = await fetch(idRequest, { headers: headers });
+    const json = await res.json();
+    const pages = Object.keys(json["query"]["pages"]);
+    if (pages.length < 1) {
+      throw new ScrapingError("Could not locate page.");
+    } else if (pages.length > 1) {
+      throw new ScrapingError(
+        `Found ${pages.length} with the name ${pageTitle}! Expected only 1.`
+      );
+    }
+    const pageId = pages[0];
+    if (pageId == -1) {
+      throw new ScrapingError(`There is currently no content for page: ${url}.`);
+    }
+    return pageId;
   }
 
-  function fetchHtml(htmlRequest) {
-    return fetch(htmlRequest, { headers: headers })
-      .then((res) => res.json())
-      .then((json) => {
-        let htmlStr = json["parse"]["text"]["*"];
-        if (!htmlStr || htmlStr.length === 0) {
-          throw new ScrapingError(`Couldn't parse html from ${url}`);
-        }
+  async function fetchWikiText(wikiTextRequest) {
+    const res = await fetch(wikiTextRequest, { headers: headers });
+    const json = await res.json();
+    let wikiTextStr = json["query"]["pages"][`${pageId}`]["revisions"][0]["slots"]["main"]["*"];
+    if (!wikiTextStr || wikiTextStr.length === 0) {
+      throw new ScrapingError(`Couldn't parse wikitext from ${url}`);
+    }
+    return wikiTextStr;
+  }
 
-        return htmlStr;
-      })
+  async function fetchHtml(htmlRequest) {
+    const res = await fetch(htmlRequest, { headers: headers });
+    const json = await res.json();
+    let htmlStr = json["parse"]["text"]["*"];
+    if (!htmlStr || htmlStr.length === 0) {
+      throw new ScrapingError(`Couldn't parse html from ${url}`);
+    }
+    return htmlStr;
   }
 
   return {
